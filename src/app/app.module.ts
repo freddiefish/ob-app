88c837d4d1553f7adeda14d88c0baf6515d7061a
Fred Feys <frefeys@gmail.com>
2019-09-06 23:59:37 +0200

    freeze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,18 @@ import { HomeComponent } from './components/home/home.component';
 
 import { LocationService } from './services/location.service';
 import { DecisionLocationsService } from './services/decision-locations.service';
-import {AgmJsMarkerClustererModule} from '@agm/js-marker-clusterer';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 import { StreetViewComponent } from './components/street-view/street-view.component';
 import { DecisionMarkersMapComponent } from './components/decision-markers-map/decision-markers-map.component';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import { environment } from '../environments/environment';
 import { DecisionMarkerModalComponent } from './components/decision-marker-modal/decision-marker-modal.component';
-import {ModalComponent} from './components/modal.component';
-import {ReadMoreComponent} from './components/read-more.component';
-import {EllipsisModule} from 'ngx-ellipsis';
+import { ModalComponent } from './components/modal.component';
+import { ReadMoreComponent } from './components/read-more.component';
+import { EllipsisModule } from 'ngx-ellipsis';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SharedModule} from './shared/shared.module';
-
-
+import { SharedModule } from './shared/shared.module';
 
 firebase.initializeApp(environment.firebase);
 
@@ -46,7 +44,7 @@ firebase.initializeApp(environment.firebase);
     AgmJsMarkerClustererModule,
     EllipsisModule,
     BrowserAnimationsModule,
-    SharedModule,
+    SharedModule
   ],
   providers: [
     LocationService,
