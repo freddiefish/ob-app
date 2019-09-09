d95e19a716e7bc0575bde44b6e2cf8d02688eb78
Fred Feys <frefeys@gmail.com>
2019-09-09 08:31:10 +0200

    clean

--- src/app/components/decisions-expansion-list/decisions-expansion-list.component.ts
+++ src/app/components/decisions-expansion-list/decisions-expansion-list.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { IDecision } from '../../models/decision-marker.model';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { throttleTime, mergeMap , scan, map, tap} from 'rxjs/operators';
 
 
 @Component({
@@ -10,16 +15,70 @@ import { MatExpansionModule } from '@angular/material/expansion';
 })
 export class DecisionsExpansionListComponent implements OnInit {
 
-  @Input()
-  decisions: IDecision[];
+  @ViewChild(CdkVirtualScrollViewport, {static: false})
+  viewport: CdkVirtualScrollViewport;
 
-  constructor() { }
+  batch = 20;
+  theEnd = false;
+
+  offset = new BehaviorSubject(null);
+  infinite: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) {
+    const batchMap = this.offset.pipe(
+      throttleTime(500),
+      mergeMap(n => this.getBatch(n)),
+      scan((acc, batch) => {
+        return { ...acc, ...batch };
+      }, {} )
+    );
+
+    this.infinite = batchMap.pipe(map(v => Object.values(v)));
+    console.log(this.infinite);
+  }
+
+  getBatch(offset) {
+    console.log(offset);
+    return this.db
+      .collection('decisions', ref =>
+         ref
+          .where('hasGeoData', '==', true)
+          .orderBy('date', 'desc')
+          .startAfter(offset)
+          .limit(this.batch)
+        )
+        .snapshotChanges()
+        .pipe(
+          tap(arr => (arr.length ? null : (this.theEnd = true))),
+          map(arr => {
+            return arr.reduce((acc, cur) => {
+              const id = cur.payload.doc.id;
+              const data = cur.payload.doc.data();
+              return { ...acc, [id]: data };
+            }, {});
+          })
+        );
+  }
+
+  nextBatch(e, offset) {
+    if (this.theEnd) {
+      return;
+    }
+
+    const end = this.viewport.getRenderedRange().end;
+    const total = this.viewport.getDataLength();
+    console.log(`${end}, '>=', ${total}`);
+    if (end === total) {
+      this.offset.next(offset);
+    }
+  }
+
+  trackByIdx(i) {
+    return i;
+  }
 
   ngOnInit() {
   }
 
-  listMoreDecisions(event) {
-    
-  }
 
 }
